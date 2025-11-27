f0b8a1e32eb5ef0d34974ab6eea5615f3eddefa4
Sannidhi.Sunkad <01fe24ica004@kletech.ac.in>
2025-11-27 21:56:48 +0530

    Added max and min score functionality

--- array.py
+++ array.py
@@ -10,10 +10,17 @@ def calculate_average(scores):
         return 0
     return sum(scores) / len(scores)
 
+def get_minimum(scores):
+    """return minimum score."""
+    return min(scores)
+
+def get_maximum(scores):
+    """return maximum score."""
+    return max(scores)
 
 
 if __name__ == "__main__":
-    print("=== Array Scores Processor (main) ===")
+    print("=== Array Scores Processor (main + ABC merged) ===")
     try:
         # Case 1: CLI arguments passed (for Jenkins)
         if len(sys.argv) > 1:
@@ -29,11 +36,14 @@ if __name__ == "__main__":
 
         total = calculate_sum(scores)
         avg = calculate_average(scores)
-       
+        min_score = get_minimum(scores)
+        max_score = get_maximum(scores)
 
         print(f"\nSum of scores = {total}")
         print(f"Average of scores = {avg:.2f}")
-   
+        print(f"Minimum score = {min_score}")
+        print(f"Maximum score = {max_score}")
 
     except ValueError:
         print("Invalid input! Please enter integer values only.")
+
